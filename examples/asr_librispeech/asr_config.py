da82bd458fce7424530262f89e221035036b6142
zzasdf <zzasdf@sjtu.edu.cn>
2024-03-28 10:25:59 +0000

    * Simplify the config * Add use custom model * Merge the setup model with model_factory * Move the inference function to slam_model_asr

--- examples/asr_librispeech/asr_config.py
+++ examples/asr_librispeech/asr_config.py
@@ -2,8 +2,10 @@ from dataclasses import dataclass, field
 from typing import Optional, List
 @dataclass
 class ModelConfig:
+    file: str = "examples/asr/model/slam_model_asr.py:model_factory"
     llm_name: str = "vicuna-13b-v1.5"
     llm_path: str = "PATH/to/LLAMA/7B"
+    llm_type: str = "decoder_only"
     llm_dim: int = 4096
     encoder_name: Optional[str] = None
     encoder_ds_rate: int = 2
@@ -11,27 +13,7 @@ class ModelConfig:
     encoder_dim: int = 1280
     encoder_projector: str = "linear"
     encoder_projector_ds_rate: int = 5
-
-    DMODEL: int = 512
-    FRONTEND_DMODEL: int = 1024   #这个是专门指moco的
-    TX_ATTENTION_HEADS: int = 8
-    TX_NUM_LAYERS: int = 6
-    PE_MAX_LENGTH: int = 500
-    AUDIO_FEATURE_SIZE: int = 1024
-    VIDEO_FEATURE_SIZE: int = 2048
-    TX_FEEDFORWARD_DIM: int = 2048
-    TX_DROPOUT: float = 0.1
-    CHAR_NUM_CLASSES: int = 40
-
-    WORD_NUM_CLASSES: int = 500
-    FRAME_LENGTH: int = 29
-    MOCO_FRONTEND_FILE: str = "/nfs/yangguanrou.ygr/AVSR/pretrain_model/moco_frontend.pt" #"/home/oss/yangguanrou.ygr/AVSR/pretrain_model/moco_frontend.pt"
-    WAV2VEC_FILE: str = "/nfs/yangguanrou.ygr/AVSR/pretrain_model/wav2vec_vox_new.pt" #"/home/oss/yangguanrou.ygr/AVSR/pretrain_model/wav2vec_vox_new.pt"
-    MAIN_REQ_INPUT_LENGTH: int = 80
-    modal: str = "AV"
-    TRAIN_LRS3_MODEL_FILE: str = "/nfs/yangguanrou.ygr/AVSR/train-step_0108-wer_0.058.ckpt"  # "/home/oss/yangguanrou.ygr/AVSR/train-step_0108-wer_0.058.ckpt"  #单一模态是这个
-    TRAINED_AO_FILE: str = "/nfs/yangguanrou.ygr/AVSR/check/train-step_0604-wer_0.054.ckpt"  #"/home/oss/yangguanrou.ygr/AVSR/check/train-step_0604-wer_0.054.ckpt"
-    TRAINED_VO_FILE: str = "/nfs/yangguanrou.ygr/AVSR/check/train-step_1191-wer_0.674.ckpt"  #"/home/oss/yangguanrou.ygr/AVSR/check/train-step_1191-wer_0.674.ckpt"
+    modal: str = "audio"
 
 @dataclass
 class PeftConfig:
@@ -98,6 +80,7 @@ class DataConfig:
     val_data_path: Optional[str] = None
     train_split: str = "train"
     test_split:str = "validation"
+    prompt: Optional[str] = None
     data_path: Optional[str] = None
     max_words: Optional[int] = None
     max_mel: Optional[float] = None
