52407bfa2c5534bed8feca6c8edef1a83af3ae85
ScorpioMiku <1056992492@qq.com>
2024-04-29 16:53:00 +0800

    training for vallex

--- examples/vallex/scripts/vallex.sh
+++ examples/vallex/scripts/vallex.sh
@@ -1,34 +1,29 @@
-export CUDA_VISIBLE_DEVICES=2
+export CUDA_VISIBLE_DEVICES=0
 export TOKENIZERS_PARALLELISM=false
 export OMP_NUM_THREADS=1
 export FORCE_CPU=1
+export RANK=0
 
-cd /home/wangtianrui/codes/SLAM-LLM-main
+cd /Work20/2023/wangtianrui/codes/util_repos/SLAM-LLM
 
 
-output_dir=/home/wangtianrui/model_save/vallex
+output_dir=/Work20/2023/wangtianrui/codes/test/debug
 
-PYTHONPATH=/home/wangtianrui/codes/SLAM-LLM-main \
-python src/slam_llm/pipeline/finetune.py \
---config-path "examples/vallex/conf" \
+PYTHONPATH=/Work20/2023/wangtianrui/codes/util_repos/SLAM-LLM \
+python examples/vallex/finetune_vallex.py \
+--config-path "/Work20/2023/wangtianrui/codes/util_repos/SLAM-LLM/examples/vallex/conf" \
 --config-name "vallex.yaml" \
 hydra.run.dir=$output_dir \
 ++model_config.llm_name="vallex" \
-++model_config.llm_path=$llm_path \
-++model_config.llm_dim=1536 \
-\
-++dataset_config.dataset=vallex_dataset \
 ++dataset_config.file="src/slam_llm/datasets/vallex_dataset.py:get_vallex_dataset" \
-++dataset_config.train_data_path=/home/wangtianrui/datas/tiny_data/data_bin \
-\
-++train_config.batching_strategy=custom \
-++train_config.use_fp16=true \
+++dataset_config.train_data_path=/Work20/2023/wangtianrui/datas/bilibli_7min_woman/data_bin_zh \
+++dataset_config.val_data_path=/Work20/2023/wangtianrui/datas/bilibli_7min_woman/data_bin_zh \
 ++train_config.warmup_steps=1000 \
 ++train_config.total_steps=100000 \
-++train_config.lr=1e-4 \
+++train_config.lr=5e-4 \
 ++train_config.validation_interval=1000 \
 ++train_config.batch_size_training=4 \
 ++train_config.val_batch_size=4 \
 ++train_config.num_workers_dataloader=1 \
-++train_config.output_dir=$output_dir \
-++metric=acc 
+++train_config.batching_strategy="custom" \
+++train_config.output_dir=$output_dir 
