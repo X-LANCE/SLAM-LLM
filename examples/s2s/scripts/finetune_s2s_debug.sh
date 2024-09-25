11ab9735a448b96b484104c952689f7fedfadbfa
cwx-worst-one <1029713857@qq.com>
2024-09-25 12:32:20 +0000

    1

--- examples/s2s/scripts/finetune_s2s_debug.sh
+++ examples/s2s/scripts/finetune_s2s_debug.sh
@@ -1,32 +1,29 @@
 #!/bin/bash
 # export PYTHONPATH=/root/whisper:$PYTHONPATH
 export PYTHONPATH=/root/fairseq:$PYTHONPATH
-# export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export CUDA_VISIBLE_DEVICES=0
 export TOKENIZERS_PARALLELISM=false
 export OMP_NUM_THREADS=1
 export LD_LIBRARY_PATH=/home/v-wenxichen/anaconda3/envs/slam/lib:$LD_LIBRARY_PATH
-export WANDB_API_KEY=406faa59cf62a3646fa3479a7e133c4cf5a77100       # please replace with your own wandb key thxxxx, unless you want to share your experiment results with me :)
 
 run_dir=/home/v-wenxichen/SLAM-LLM
 cd $run_dir
 code_dir=examples/s2s
 
-speech_encoder_path="/home/v-wenxichen/.cache/whisper/small.pt"   # whisper small
-llm_path="/home/v-wenxichen/.cache/huggingface/hub/models--Qwen--Qwen2-0.5B"
-
+speech_encoder_path="small"   # whisper small
+llm_path="Qwen/Qwen2-0.5B"
 # train_data_path=/home/v-wenxichen/data/s2s/test/test_train.jsonl
 # val_data_path=/home/v-wenxichen/data/s2s/test/test_val.jsonl
-train_data_path="/valleblob/v-wenxichen/data/s2s/VoiceAssistant-400K"
-val_data_path="/valleblob/v-wenxichen/data/s2s/VoiceAssistant-400K"
+train_data_path="gpt-omni/VoiceAssistant-400K"
+val_data_path="gpt-omni/VoiceAssistant-400K"
 
-exp_name="s2s_train_v0_test"
+exp_name="s2s_train_v0"
 # exp_name="debug"
 
-home_dir=/valleblob
+home_dir=/home/v-wenxichen/exp/s2s
 # output_dir=$home_dir/$(TZ='Asia/Shanghai' date +"%Y_%m_%d")/$(TZ='Asia/Shanghai' date +"%H_%M_%S")
 output_dir=$home_dir/$(TZ='Asia/Shanghai' date +"%Y_%m_%d")/$exp_name
-# ckpt_path=/home/v-wenxichen/exp/s2s/2024_09_23/s2s_train_test/s2s_epoch_1_step_15000  # this line is for resuming training
+ckpt_path=/home/v-wenxichen/exp/s2s/2024_09_23/s2s_train_test/s2s_epoch_1_step_15000
 
 if [ "$exp_name" = "debug" ]; then
     use_wandb=false
@@ -100,7 +97,7 @@ else
         --config-path "conf" \
         --config-name "prompt.yaml" \
         ++train_config.enable_ddp=true \
-        ++train_config.enable_fsdp=false \
+        ++train_config.enable_fsdp=false \7
         $hydra_args
 fi
 
