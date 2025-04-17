0a11c0a34aa97255b461b2519986b821512465a7
teamtee <2624071330@qq.com>
2025-04-17 11:12:07 +0800

    Add:An example, aispeech_asr, and a large dataset, speech_dataset_large, have been added, supporting multi-machine multi-GPU decoding.

--- src/slam_llm/pipeline/inference_batch_deepspeed.py
+++ src/slam_llm/pipeline/inference_batch_deepspeed.py
@@ -4,11 +4,12 @@ import fire
 import deepspeed
 import random
 import importlib
-
+from tqdm import tqdm
 # nn
 import torch
+import torch_npu
 from transformers.models.llama.modeling_llama import LlamaDecoderLayer
-
+from torch.utils.data import DistributedSampler
 # opt
 import torch.optim as optim
 from torch.optim.lr_scheduler import StepLR
@@ -19,7 +20,7 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 
 from torch.distributed.fsdp.fully_sharded_data_parallel import CPUOffload
 from slam_llm.policies import AnyPrecisionAdamW, apply_fsdp_checkpointing
-
+import torch.distributed as dist
 # config
 # from llama_recipes.configs import fsdp_config as FSDP_CONFIG
 # from llama_recipes.configs import train_config as TRAIN_CONFIG
@@ -49,7 +50,7 @@ import hydra
 from omegaconf import DictConfig, ListConfig, OmegaConf
 from pathlib import Path
 
-@hydra.main(config_name=None, version_base=None)
+@hydra.main(config_name=None, version_base=None)  # strict=False 允许忽略未知参数)
 def main_hydra(cfg: DictConfig):
     def to_plain_list(cfg_item):
         if isinstance(cfg_item, ListConfig):
@@ -77,11 +78,10 @@ def main(kwargs: DictConfig):
     # train_config, fsdp_config, model_config, log_config = TRAIN_CONFIG(), FSDP_CONFIG(), MODEL_CONFIG(), LOG_CONFIG()
     # update_config((train_config, fsdp_config, model_config, log_config), **kwargs)
 
-    train_config, model_config, log_config, dataset_config, deepspeed_config = kwargs.train_config, \
+    train_config, model_config, log_config, dataset_config = kwargs.train_config, \
                                                                           kwargs.model_config, \
                                                                           kwargs.log_config, \
-                                                                          kwargs.dataset_config, \
-                                                                          kwargs.deepspeed_config
+                                                                          kwargs.dataset_config
     del kwargs.train_config
     del kwargs.model_config
     del kwargs.log_config
@@ -113,7 +113,7 @@ def main(kwargs: DictConfig):
 
 
     # Set the seeds for reproducibility
-    torch.npu.manual_seed(train_config.seed)
+    torch_npu.npu.manual_seed(train_config.seed)
     torch.manual_seed(train_config.seed)
     random.seed(train_config.seed)
 
@@ -122,9 +122,9 @@ def main(kwargs: DictConfig):
     world_size = int(os.environ["WORLD_SIZE"])
     logger.info(f"local_rank: {local_rank}, rank: {rank}, world_size: {world_size}")
 
-    torch.npu.set_device(local_rank)
-    clear_gpu_cache(local_rank)
-    setup_environ_flags(rank)
+    deepspeed.init_distributed(
+        dist_backend='hccl',    # 使用NCCL后端（GPU场景）
+    )
 
     if rank == 0:
         logger.info("train_config: {}".format(train_config))
@@ -142,108 +142,51 @@ def main(kwargs: DictConfig):
 
     model_factory = get_custom_model_factory(model_config, logger)
     model, tokenizer = model_factory(train_config, model_config, **kwargs)
-    parameters = filter(lambda p: p.requires_grad, model.parameters())
-    device = torch.device("npu" if torch.npu.is_available() else "cpu")
-
-    # If you are facing problem from limited memory(<=256GB), you can try to replace the above code with the following code
-    # for i in range(rank):
-    #     while not os.path.isfile(f".{i}.done"):
-    #         pass
-    # assert not os.path.isfile(f".{rank}.done"), f".{rank}.done already exists!"
-    # model_factory = get_custom_model_factory(model_config, logger)
-    # model, tokenizer = model_factory(train_config, model_config, **kwargs)
-    # parameters = filter(lambda p: p.requires_grad, model.parameters())
-    # device = torch.device("npu" if torch.npu.is_available() else "cpu")
-    # model.half()
-    # with open(f".{rank}.done", "w"):
-    #     pass
-
-
-    # Initialize the optimizer and learning rate scheduler
-    model_engine, _, _, _ = deepspeed.initialize(
-        model=model, model_parameters=parameters, config=deepspeed_config
-    )
-
-    
-    # Convert the model to bfloat16 if fsdp and pure_bf16 is enabled
-    # if (train_config.enable_fsdp or train_config.enable_ddp) and fsdp_config.pure_bf16:
-    #     model.to(torch.bfloat16)
-
-    #setting up FSDP if enable_fsdp is enabled
-    # if train_config.enable_ddp:
-    #     model = model.npu(local_rank)
-    #     model = DDP(model, device_ids=[local_rank],
-    #                 find_unused_parameters=kwargs.get("train_conf", {}).get("find_unused_parameters", False))
-    # elif not train_config.quantization:
-    #     model.to(device)
-
-    # dataset_config = generate_dataset_config(train_config, kwargs)
+    device = torch.device(f"npu:{local_rank}" if torch.npu.is_available() else "cpu") # FIX(MZY): put the whole model to device.
+    model.to(device)
+    model.eval()
     logger.info("dataset_config: {}".format(dataset_config))
-    if rank == 0:
-        if log_config.use_wandb:
-            wandb.config.update({"dataset_config": dataset_config})
-    
-    # Load and preprocess the dataset for training and validation
-    dataset_train = get_preprocessed_dataset(
+    dataset_test = get_preprocessed_dataset(
         tokenizer,
         dataset_config,
-        split="train",
+        split="test",
     )
-    if (not (train_config.enable_fsdp or train_config.enable_ddp) or rank == 0) and train_config.batching_strategy != "dynamic":
-        logger.info(f"--> Training Set Length = {len(dataset_train)}")
-    dataset_val = get_preprocessed_dataset(
-        tokenizer,
-        dataset_config,
-        split="val",
-    )
-    if not (train_config.enable_fsdp or train_config.enable_ddp) or rank == 0 and train_config.batching_strategy != "dynamic":
-        logger.info(f"--> Validation Set Length = {len(dataset_val)}")
-    if train_config.batching_strategy == "packing":
-        dataset_train = ConcatDataset(dataset_train, chunk_size=train_config.context_length)
-
-    train_dl_kwargs = get_dataloader_kwargs(train_config, dataset_train, tokenizer, "train")
-
-    # Create DataLoaders for the training and validation dataset
-    train_dataloader = torch.utils.data.DataLoader(
-        dataset_train,
-        num_workers=train_config.num_workers_dataloader,
-        pin_memory=True,
-        **train_dl_kwargs,
-    )
-
-    eval_dataloader = None
-    if train_config.run_validation:
-        if train_config.batching_strategy == "packing":
-            dataset_val = ConcatDataset(dataset_val, chunk_size=train_config.context_length)
-
-        val_dl_kwargs = get_dataloader_kwargs(train_config, dataset_val, tokenizer, "val")
-
-        eval_dataloader = torch.utils.data.DataLoader(
-            dataset_val,
+    # sampler = DistributedSampler(
+    #             dataset_test,
+    #             rank=dist.get_rank(),
+    #             num_replicas=dist.get_world_size(),
+    #         )
+    test_dataloader = torch.utils.data.DataLoader(
+            dataset_test,
             num_workers=train_config.num_workers_dataloader,
             pin_memory=True,
-            **val_dl_kwargs,
+            shuffle=False,
+            batch_size=train_config.val_batch_size,
+            drop_last=False,
+            collate_fn=dataset_test.collator,
+            # sampler=sampler
+            # multiprocessing_context=mp.get_context("spawn")
         )
 
-
-    # Start the training process
-    results = train(
-        model_engine,
-        train_dataloader,
-        eval_dataloader,
-        tokenizer,
-        train_config.gradient_accumulation_steps,
-        train_config,
-        log_config,
-        local_rank,
-        rank,
-    )
-    if rank==0:
-        [logger.info(f'Key: {k}, Value: {v}') for k, v in results.items()]
-
-    if rank == 0:
-        if log_config.use_wandb:
-            wandb.finish()
-
+    logger.info("=====================================")
+    pred_path = kwargs.get('decode_log') + f"_pred"
+    gt_path = kwargs.get('decode_log') + f"_gt"
+    pred_result = ""
+    gt_result = ""
+    with torch.no_grad():
+        for step, batch in tqdm(enumerate(test_dataloader)):
+            for key in batch.keys():
+                batch[key] = batch[key].to(device) if isinstance(batch[key], torch.Tensor) else batch[key]
+            model_outputs = model.generate(**batch)
+            if hasattr(model, 'tokenizer'):
+                output_text = model.tokenizer.batch_decode(model_outputs, add_special_tokens=False, skip_special_tokens=True)
+            else:
+                output_text = tokenizer.batch_decode(model_outputs, skip_special_tokens=True)
+            for key, text, target in zip(batch["keys"], output_text, batch["targets"]):
+                pred_result += key + " " + text.strip() + "\n"
+                gt_result += key + " " + target + "\n"
+    with open(pred_path, "a+") as pred, open(gt_path, "a+") as gt:
+        pred.write(pred_result)
+        gt.write(gt_result)
 if __name__ == "__main__":
     main_hydra()
